1182427091b3fd89119eb7025c5ac1124ecc0fe4
stansstanman <ruggero.basanisi@gmail.com>
2023-06-06 15:42:31 +0200

    updated gitignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+
+__version__ = '0.0.1'
+NAME = 'TweakDreams'
+AUTHOR = 'Ruggero Basanisi'
+MAINTEINER = 'Ruggero Basanisi'
+
+setup(
+    name=NAME,
+    version=__version__,
+    packages=find_packages(),
+    author=AUTHOR,
+    maintainer=MAINTEINER,
+    description="Pipeline for TweakDreams project's data analysis",
+    license='None',
+    install_requires=['numpy',
+                      'scipy',
+                      'mne']
+)
