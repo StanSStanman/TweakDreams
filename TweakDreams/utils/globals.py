349b158708cb16bf1cdee18f934b6f45f1ecec7e
stansstanman <ruggero.basanisi@gmail.com>
2024-01-30 11:58:32 +0000

    changed globals project dir

--- TweakDreams/utils/globals.py
+++ TweakDreams/utils/globals.py
@@ -1,3 +1,3 @@
 global prj_data
-prj_data = '/media/jerry/ruggero/tweakdreams'
-# prj_data = '/home/ruggero.basanisi/data/tweakdreams'
+# prj_data = '/media/jerry/ruggero/tweakdreams'
+prj_data = '/home/ruggero.basanisi/data/tweakdreams'
